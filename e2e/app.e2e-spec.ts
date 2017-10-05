33ec874fa625595a486a808a2274e3289d488438
Kyle Annen <kyleannen@gmail.com>
2017-10-04 21:49:33 -0500

    Initialize git repository

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('angulartictactoe App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('Angular Tic Tac Toe');
   });
 });
