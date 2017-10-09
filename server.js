584e9034b9f543e5f374a2748dc4a3961c228a10
Kyle Annen <kyleannen@gmail.com>
2017-10-09 15:31:46 -0500

    set server to use HTTPS

--- server.js
+++ server.js
@@ -4,6 +4,19 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+const forceSSL = function() {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+        ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+};
+
+app.use(forceSSL());
+
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
