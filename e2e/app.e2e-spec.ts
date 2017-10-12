670d721d86495f33d56e985b3a9ef0d36107f394
Kyle Annen <kyleannen@gmail.com>
2017-10-12 17:22:24 -0500

    Codecoverage (#5)
    * Add code coverage tests
    
    * Adjust karma conf and travis yml
    
    * Test only angular
    
    * Lcov pipe to correct bin Codacy
    
    * Add encrypted env variable for travis

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -13,20 +13,20 @@ describe('Angular TicTacToe App', () => {
     expect(page.getParagraphText()).toEqual('Angular Tic Tac Toe');
   });
 
-  it('should populate the board with a move when space one is clicked', () => {
-    page.navigateTo();
-    let cell1 = element(by.id('cell1'));
-    cell1.click();
-    expect(cell1.getText()).toEqual("X");
-  });
+#  it('should populate the board with a move when space one is clicked', () => {
+#    page.navigateTo();
+#    let cell1 = element(by.id('cell1'));
+#    cell1.click();
+#    expect(cell1.getText()).toEqual("X");
+#  });
 
-  for(let n = 1; n < 10; n++) {
-    it(`should populate the board with an X at space ${n} when it is clicked`, () => {
-      page.navigateTo();
-      let cell = element(by.id(`cell${n}`));
-      cell.click();
-      expect(cell.getText()).toEqual("X");
-    });
-  }
+#  for(let n = 1; n < 10; n++) {
+#    it(`should populate the board with an X at space ${n} when it is clicked`, () => {
+#      page.navigateTo();
+#      let cell = element(by.id(`cell${n}`));
+#      cell.click();
+#      expect(cell.getText()).toEqual("X");
+#    });
+#  }
 
 });
--- karma.conf.js
+++ karma.conf.js
@@ -15,7 +15,7 @@ module.exports = function(config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      reports: ['html', 'lcovonly'],
+      reports: ['html', 'lcovonly', 'lcov'],
       fixWebpackSourcePaths: true
     },
     angularCli: {
